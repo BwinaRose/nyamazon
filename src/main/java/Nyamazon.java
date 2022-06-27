e5fc537b53f32558519910d9eb0c41473d7495fd
BwinaRose <sabrinakrueger24@gmail.com>
2022-06-27 10:12:45 -0400

    account testing

--- src/main/java/Nyamazon.java
+++ src/main/java/Nyamazon.java
@@ -1,5 +1,6 @@
 import java.util.List;
 import java.util.Scanner;
+import java.util.regex.Pattern;
 
 public class Nyamazon {
     private List<Customer> customers;
@@ -16,21 +17,36 @@ public class Nyamazon {
         Account account;
         Boolean signedIn;
         int input;
-        System.out.println("Nyamazon the {Program}\nType:\n0 To Sign In\n" +
+        System.out.println("\n Welcome to Nyamazon\n    |\\__/,|   (`\\\n" +
+                "  _.|o o  |_   ) )\n" +
+                "-(((---(((--------\n\n  Type:\n0 To Sign In\n" +
                 "1 To Sign Up as Vendor\n2 To Sign Up as Customer\n3 Exit");
         input = Integer.parseInt(scanner.nextLine());
         switch(input) {
             case 0:
-
                 break;
             case 1:
                 break;
-
+            case 2:
+                break;
+            case 3:
+                break;
         }
+    }
+
+    public static boolean patternMatches(String emailAddress, String regexPattern) {
+        return Pattern.compile(regexPattern)
+                .matcher(emailAddress)
+                .matches();
     }
 
     public void signIn(String email,String password){
         System.out.println("Enter e-mail");
+        String regexPat = "^(?=.{1,64}@)[A-Za-z0-9_-]+(\\.[A-Za-z0-9_-]+)*@[^-][A-Za-z0-9-]+(\\.[A-Za-z0-9-]+)*(\\.[A-Za-z]{2,})$";
+        email = scanner.nextLine();
+        if (email.matches(regexPat)){
+
+        }
 
     }
 
@@ -46,6 +62,10 @@ public class Nyamazon {
 
     }
 
+    public static void main(String[] args) {
+        Nyamazon nya = new Nyamazon();
+        nya.startMenu();
+    }
 
 
 }
---
+++ src/test/java/NyamazonTest.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.BeforeEach;
+
+public class NyamazonTest {
+    @BeforeEach void setUp(){
+
+    }
+}
+
+
+
